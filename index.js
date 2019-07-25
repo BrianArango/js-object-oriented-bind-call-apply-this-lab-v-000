2215856ab1780474617b076ddc5e1fcaa78cbd73
Brian Arango <brianvictus@gmail.com>
2019-07-25 04:24:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-justInvoke = function() {
-  return this;
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, argument) {
+  return fn.call(thisValue, argument);
+}
+
+function setThisWithApply(fn, thisValue, ourArguments) {
+  return fn.apply(thisValue, ourArguments);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
 }
